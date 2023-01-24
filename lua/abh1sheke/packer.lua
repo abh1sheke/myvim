2181137dceaac35089a575ff30a14b61eb8b7077
Abhisheke <abhisheke.acharya@gmail.com>
2023-01-24 13:50:24 +0530

    add treesitter

--- lua/abh1sheke/packer.lua
+++ lua/abh1sheke/packer.lua
@@ -2,19 +2,22 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-	
-  use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	requires = { {'nvim-lua/plenary.nvim'} }
-      }
-  use ({ 
-	  "catppuccin/nvim", 
-	  as = "catppuccin", 
-	  config = function()
-	  	vim.cmd("colorscheme catppuccin")
-	  end
-       })
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use ({ 
+		"catppuccin/nvim", 
+		as = "catppuccin", 
+		config = function()
+			vim.cmd("colorscheme catppuccin")
+		end
+	})
+
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
