a854cdf7b1e9f7f51ce5f7a42d80bd41e1205ba6
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-02-15 18:34:50 +0530

    add trouble.nvim and remaps

--- lua/abh1sheke/packer.lua
+++ lua/abh1sheke/packer.lua
@@ -2,47 +2,55 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use("rebelot/kanagawa.nvim")
-    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
+        use {
+            'nvim-telescope/telescope.nvim', tag = '0.1.1',
+            -- or                            , branch = '0.1.x',
+            requires = { { 'nvim-lua/plenary.nvim' } }
         }
-    }
-    -- Prettier deps
-    use('jose-elias-alvarez/null-ls.nvim')
-    use('MunifTanjim/prettier.nvim')
+        use("rebelot/kanagawa.nvim")
+        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+        use('theprimeagen/harpoon')
+        use('mbbill/undotree')
+        use('tpope/vim-fugitive')
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            branch = 'v1.x',
+            requires = {
+                -- LSP Support
+                { 'neovim/nvim-lspconfig' },
+                { 'williamboman/mason.nvim' },
+                { 'williamboman/mason-lspconfig.nvim' },
+                -- Autocompletion
+                { 'hrsh7th/nvim-cmp' },
+                { 'hrsh7th/cmp-nvim-lsp' },
+                { 'hrsh7th/cmp-buffer' },
+                { 'hrsh7th/cmp-path' },
+                { 'saadparwaiz1/cmp_luasnip' },
+                { 'hrsh7th/cmp-nvim-lua' },
 
-    use {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-        end
-    }
-    use('lukas-reineke/indent-blankline.nvim')
-end)
+                { 'L3MON4D3/LuaSnip' },
+                { 'rafamadriz/friendly-snippets' },
+            }
+        }
+        -- Prettier deps
+        use('jose-elias-alvarez/null-ls.nvim')
+        use('MunifTanjim/prettier.nvim')
+
+        use {
+            'lewis6991/gitsigns.nvim',
+            config = function()
+                require('gitsigns').setup()
+            end
+        }
+        use('lukas-reineke/indent-blankline.nvim')
+        use {
+            "folke/trouble.nvim",
+            requires = "nvim-tree/nvim-web-devicons",
+            config = function()
+                require("trouble").setup {
+                }
+            end
+        }
+    end)
