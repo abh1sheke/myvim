587544dbb3a49c7fc1d3965559d9d6bd2a97eaf3
Abhisheke Acharya <abhisheke.acharya@gmail.com>
2023-02-16 12:08:07 +0530

    add nvim tree mappings and open_on_setup

--- lua/abh1sheke/init.lua
+++ lua/abh1sheke/init.lua
@@ -1,2 +1,10 @@
 require("abh1sheke.remap")
 require("abh1sheke.set")
+
+
+local function open_nvim_tree()
+  require("nvim-tree.api").tree.open()
+end
+
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree } )
--- lua/abh1sheke/remap.lua
+++ lua/abh1sheke/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -18,7 +18,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>pp", ":Prettier")
+vim.keymap.set("n", "<leader>pp", vim.cmd.Prettier)
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
